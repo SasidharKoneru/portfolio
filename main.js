e863b8024ddb461a483d5a837171144ed13ee9b1
Koneru Sasidhar <141036612+SasidharKoneru@users.noreply.github.com>
2024-09-30 17:58:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+/*==== Toggle icon navbar ==== */
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+            });
+        };
+    });
+
+    /*===== Sticky navbar====== */
+    let header = document.querySelector('header');
+    header.classList.toogle('sticky',window.scrollY > 100);
+
+    /* ====== remove toggle icon and navbar ======= */
+    menuIcon.classList.remove('fa-xamrk');
+    navbar.classList.remmove('active');
+
+};
